548fc9b8e0e23c1b61fc80113fbccc89cc3de01d
Mahmadullo <abdulloev156380@gmail.com>
2021-03-15 21:36:40 +0500

    beginning to do something awesome with 3 objects

---
+++ app/Models/Contact.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    public function phones()
+    {
+        return $this->hasMany(Phone::class);
+    }
+
+    public function emails()
+    {
+        return $this->hasMany(Email::class);
+    }
+}
---
+++ database/migrations/2021_03_15_161120_create_phones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contact_id')->constrained()->onDelete('cascade');
+            $table->string('content')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
