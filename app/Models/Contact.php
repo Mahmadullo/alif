c57a91ac9651c70e8dc60880e0f3da5a18da15a3
Mahmadullo <abdulloev156380@gmail.com>
2021-03-16 09:38:09 +0500

    Enabling create via attributes

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+
+    protected $guarded = [];
+    
     use HasFactory;
 
     public function phones()
--- app/Models/Email.php
+++ app/Models/Email.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
+    protected $guarded = [];
+        
     use HasFactory;
 
     public function contact()
